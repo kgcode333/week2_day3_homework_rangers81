6ada5e61a1e21d98b4075976249ac3008884a010
kgcode333 <kimegriffin333@gmail.com>
2022-01-13 08:02:30 -0600

    kims shopping cart

---
+++ calculation.py
@@ -0,0 +1,10 @@
+
+word = input("put a word to check if palindrome")
+reverse = word[::-1]
+if reverse == word:
+    print(True)
+else:
+    print(False)
+
+def backwards(word):
+    palindrome = "racecar"    
---
+++ formulas.py
@@ -0,0 +1,12 @@
+def square_footage(length,width):
+    length = int(input("Enter the length of house: "))
+    width = int(input("Enter the width of house: "))
+    area = length * width
+    print(area) 
+
+
+def circle(radius):
+    pi = 3.14
+    radius = int(input("Enter the radius of a circle: "))
+    circumference = 2 * pi * radius
+    print(circumference)
